1f68f6f8c42317e026e18c772865d7089f126ee1
Jimmy Chu <jimmychu0807@gmail.com>
2019-03-03 18:14:41 +0800

    Enhancement on generating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'factory_bot'
 
+GENERATED_LINKS = 55
+
 3.times do
   user = FactoryBot.create :user
   link = FactoryBot.create :link, user: user
@@ -8,3 +10,8 @@ end
 second_user = User.second
 first_link = Link.first
 second_user.votes.create!(link: first_link)
+
+first_user = User.first
+(GENERATED_LINKS - 3).times do |i|
+  FactoryBot.create :link, user: first_user
+end
